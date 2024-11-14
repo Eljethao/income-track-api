5f93ae30b4e2175fef7512d24ea17dc46146bbdf
lailaolabdev <lailaolabdev@gmail.com>
2024-11-14 23:39:32 +0700

    add health route

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.use(express.json());
 
 app.use("v1/api/users", userRoutes);
 
+app.use('/health', (req,res) => {
+    res.status(200).json({
+        message: "Server is running"
+    })
+})
+
 const PORT = process.env.PORT || 7070;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
